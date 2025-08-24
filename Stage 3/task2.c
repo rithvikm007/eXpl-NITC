767f851d36de6cbf65f6826d76d888766a12b64f
rithvikm007 <rithvikreddym2006@gmail.com>
2025-08-25 00:52:13 +0530

    Task 2 Done

---
+++ Stage 3/task2.c
@@ -0,0 +1,294 @@
+#include <string.h>
+
+struct AST_Node *makeVariableLeafNode(Type type, char varname, char *s)
+{
+    struct AST_Node *new_node = (struct AST_Node *)malloc(sizeof(struct AST_Node));
+    new_node->s = (char *)malloc(sizeof(char) * strlen(s));
+    new_node->s = strdup(s);
+    new_node->nodetype = VARIABLE;
+    new_node->type = type;
+    new_node->varname = (char *)malloc(sizeof(char));
+    new_node->varname[0] = varname;
+    new_node->left = (struct AST_Node *)NULL;
+    new_node->right = (struct AST_Node *)NULL;
+    new_node->mid = (struct AST_Node *)NULL;
+    return new_node;
+}
+
+struct AST_Node *makeConstantLeafNode(Type type, int val, char *s)
+{
+    struct AST_Node *new_node = (struct AST_Node *)malloc(sizeof(struct AST_Node));
+    new_node->s = (char *)malloc(sizeof(char) * strlen(s));
+    new_node->s = strdup(s);
+    new_node->nodetype = CONSTANT;
+    new_node->type = type;
+    new_node->val = val;
+    new_node->left = (struct AST_Node *)NULL;
+    new_node->right = (struct AST_Node *)NULL;
+    new_node->mid = (struct AST_Node *)NULL;
+    new_node->varname = (char *)NULL;
+    return new_node;
+}
+
+struct AST_Node *makeNode(Nodetype node_type, Type type, struct AST_Node *l, struct AST_Node *m, struct AST_Node *r, char *s)
+{
+    struct AST_Node *new_node = (struct AST_Node *)malloc(sizeof(struct AST_Node));
+
+    // Type checking
+    if (node_type == OPERATOR && type == INTEGER)
+    {
+        if (l->type == BOOLEAN || r->type == BOOLEAN)
+        {
+            printf("Error: int mismatch 1\n");
+            exit(1);
+        }
+    }
+    if (node_type == WHILE || node_type == IF)
+    {
+        if (l->type == INTEGER)
+        {
+            printf("Error: int mismatch 2\n");
+            exit(1);
+        }
+    }
+
+    new_node->s = (char *)malloc(sizeof(char) * strlen(s));
+    new_node->s = strdup(s);
+    new_node->nodetype = node_type;
+    new_node->type = type;
+    new_node->left = l;
+    new_node->mid = m;
+    new_node->right = r;
+    new_node->varname = (char *)NULL;
+    return new_node;
+}
+
+void printIndent(int depth, int isRight) {
+    for (int i = 0; i < depth - 1; i++) {
+        printf("    ");
+    }
+    if (depth > 0) {
+        printf(isRight ? "└─ " : "├─ ");
+    }
+}
+
+void print_tree(struct AST_Node *root, int lvl, int isRight)
+{
+	for(int i = 0; i < lvl; i++) printf(" ");
+    if (root)
+    {
+    	printIndent(lvl, isRight);
+        printf("%s\n", root->s);
+        if (root->left != NULL)
+        	print_tree(root->left, lvl + 1, 0);
+        if (root->right != NULL)
+        	print_tree(root->right, lvl + 1, 1);
+    }
+}
+
+
+//--------------------------------Reg Functions Start
+int free_reg = -1;
+
+int getReg()
+{
+    free_reg++;
+    return free_reg;
+}
+
+void freeReg()
+{
+    free_reg--;
+}
+//--------------------------------Reg Functions End
+
+//--------------------------------Code Gen Functions Start
+int codeGen(struct AST_Node *t, FILE *target_file)
+{
+    int p, q, r, s, addr;
+
+    if (t->nodetype == CONSTANT)
+    {
+        p = getReg();
+        fprintf(target_file, "MOV R%d, %d\n", p, t->val);
+        return p;
+    }
+
+    if (t->nodetype == VARIABLE)
+    {
+        p = getReg();
+        addr = getAddr(t->varname);
+        fprintf(target_file, "MOV R%d, [%d]\n", p, addr);
+        return p;
+    }
+
+    if (t->nodetype == READ)
+    {
+        p = getReg();
+        addr = getAddr(t->left->varname);
+        fprintf(target_file, "MOV R%d, \"Read\"\n", p);
+        fprintf(target_file, "PUSH R%d\n", p);
+        fprintf(target_file, "MOV R%d, -1\n", p);
+        fprintf(target_file, "PUSH R%d\n", p);
+        fprintf(target_file, "MOV R%d, %d\n", p, addr);
+        fprintf(target_file, "PUSH R%d\n", p);
+        fprintf(target_file, "PUSH R%d\n", p);
+        fprintf(target_file, "PUSH R%d\n", p);
+        fprintf(target_file, "CALL 0\n");
+        fprintf(target_file, "POP R%d\n", p);
+        fprintf(target_file, "POP R%d\n", p);
+        fprintf(target_file, "POP R%d\n", p);
+        fprintf(target_file, "POP R%d\n", p);
+        fprintf(target_file, "POP R%d\n", p);
+        freeReg();
+        return -1;
+    }
+
+    if (t->nodetype == WRITE)
+    {
+        p = codeGen(t->left, target_file);
+        q = getReg();
+        fprintf(target_file, "MOV R%d, \"Write\"\n", q);
+        fprintf(target_file, "PUSH R%d\n", q);
+        fprintf(target_file, "MOV R%d, -2\n", q);
+        fprintf(target_file, "PUSH R%d\n", q);
+        fprintf(target_file, "PUSH R%d\n", p);
+        fprintf(target_file, "PUSH R%d\n", q);
+        fprintf(target_file, "PUSH R%d\n", q);
+        fprintf(target_file, "CALL 0\n");
+        fprintf(target_file, "POP R%d\n", q);
+        fprintf(target_file, "POP R%d\n", q);
+        fprintf(target_file, "POP R%d\n", q);
+        fprintf(target_file, "POP R%d\n", q);
+        fprintf(target_file, "POP R%d\n", q);
+        freeReg();
+        freeReg();
+        return -1;
+    }
+
+    if (t->nodetype == STATEMENT)
+    {
+        codeGen(t->left, target_file);
+        codeGen(t->right, target_file);
+        return -1;
+    }
+
+    if (t->nodetype == OPERATOR)
+    {
+        if (t->type == BOOLEAN)
+        {
+            p = codeGen(t->left, target_file);
+            q = codeGen(t->right, target_file);
+            if (strcmp(t->s, ">") == 0)
+            {
+                fprintf(target_file, "GT R%d, R%d\n", p, q);
+            }
+            else if (strcmp(t->s, "<") == 0)
+            {
+                fprintf(target_file, "LT R%d, R%d\n", p, q);
+            }
+            else if (strcmp(t->s, ">=") == 0)
+            {
+                fprintf(target_file, "GE R%d, R%d\n", p, q);
+            }
+            else if (strcmp(t->s, "<=") == 0)
+            {
+                fprintf(target_file, "LE R%d, R%d\n", p, q);
+            }
+            else if (strcmp(t->s, "==") == 0)
+            {
+                fprintf(target_file, "EQ R%d, R%d\n", p, q);
+            }
+            else if (strcmp(t->s, "!=") == 0)
+            {
+                fprintf(target_file, "NE R%d, R%d\n", p, q);
+            }
+            freeReg();
+            return p;
+        }
+
+        if (t->s[0] == '=')
+        {
+            p = codeGen(t->right, target_file);
+            addr = getAddr(t->left->varname);
+            fprintf(target_file, "MOV [%d], R%d\n", addr, p);
+            freeReg();
+            return -1;
+        }
+
+        else
+        {
+            p = codeGen(t->left, target_file);
+            q = codeGen(t->right, target_file);
+            switch (t->s[0])
+            {
+            case '+':
+                fprintf(target_file, "ADD R%d, R%d\n", p, q);
+                freeReg();
+                return p;
+            case '-':
+                fprintf(target_file, "SUB R%d, R%d\n", p, q);
+                freeReg();
+                return p;
+            case '*':
+                fprintf(target_file, "MUL R%d, R%d\n", p, q);
+                freeReg();
+                return p;
+            case '/':
+                fprintf(target_file, "DIV R%d, R%d\n", p, q);
+                freeReg();
+                return p;
+            }
+        }
+    }
+
+    else if (t->nodetype == WHILE)
+    {
+        s = getLabel();
+        fprintf(target_file, "L%d:\n", s);
+        p = codeGen(t->left, target_file);
+        r = getLabel();
+        fprintf(target_file, "JZ R%d, L%d\n", p, r);
+        freeReg();
+        p = codeGen(t->right, target_file);
+        fprintf(target_file, "JMP L%d\n", s);
+        fprintf(target_file, "L%d:\n", r);
+        return p;
+    }
+
+    else if (t->nodetype == IF)
+    {
+        p = codeGen(t->left, target_file);
+        s = getLabel();
+        fprintf(target_file, "JZ R%d, L%d\n", p, s);
+        freeReg();
+        p = codeGen(t->mid, target_file);
+        if (t->right != NULL)
+        {
+            r = getLabel();
+            fprintf(target_file, "JMP L%d\n", r);
+            fprintf(target_file, "L%d:\n", s);
+            p = codeGen(t->right, target_file);
+            fprintf(target_file, "L%d:\n", r);
+        }
+        else
+        {
+            fprintf(target_file, "L%d:\n", s);
+        }
+        return p;
+    }
+}
+//--------------------------------Code Gen Functions End
+
+//--------------------------------Utility Functions
+int getAddr(char *c)
+{
+    return 4096 + (*c - 'a');
+}
+
+int label = 0;
+
+int getLabel()
+{
+    return label++;
+}
---
+++ Stage 3/task2.y
@@ -0,0 +1,97 @@
+%{
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include "task2.h"
+	#include "task2.c"
+
+    extern FILE* yyin;
+	extern char* yytext;
+
+    void yyerror(char const *s);
+	int yylex(void);
+%}
+
+%union{
+	struct AST_Node *node;
+}
+
+%type <node> program stmt_list stmt expr InputStmt OutputStmt AsgStmt IfStmt WhileStmt ID_ NUM_
+%token PLUS_ MINUS_ MUL_ DIV_ LT_ GT_ LE_ GE_ NE_ EQ_
+%token BEGIN_ END_ READ_ WRITE_ IF_ THEN_ ELSE_ ENDIF_ WHILE_ DO_ ENDWHILE_
+%token ID_ NUM_
+%left LT_ GT_ LE_ GE_ NE_ EQ_
+%left PLUS_ MINUS_
+%left MUL_ DIV_
+
+%%
+
+program : BEGIN_ stmt_list END_ {
+								$$ = $2;
+
+								print_tree($2, 0, 0);
+
+								FILE *fp = fopen("code.xsm", "w");
+								fprintf(fp, "0\n2056\n0\n0\n0\n0\n0\n0\n");
+								fprintf(fp, "MOV SP, 4122\n");
+								int p = codeGen($2, fp);
+								fprintf(fp, "INT 10\n");
+
+          						fclose(fp);
+
+								exit(0);
+							}
+		| BEGIN_ END_ {
+			printf("Empty Program\n");
+			printf("Parsing Successful\n");
+			exit(1);
+		}
+
+stmt_list: stmt_list stmt ';' {$$ = makeNode(STATEMENT, VOID, $1, NULL, $2, "STATEMENT");}
+	| stmt ';' {$$ = $1;}
+
+stmt: InputStmt
+    | OutputStmt
+    | AsgStmt
+	| IfStmt
+	| WhileStmt
+	
+InputStmt: READ_ '(' ID_ ')' { $$ = makeNode(READ, VOID, $3, NULL, NULL, "READ");}
+
+OutputStmt: WRITE_ '(' expr ')' { $$ = makeNode(WRITE, VOID, $3, NULL, NULL, "WRITE");}
+
+AsgStmt: ID_ '=' expr { $$ = makeNode(OPERATOR, INTEGER, $1, NULL, $3, "="); }
+
+IfStmt: IF_ '(' expr ')' THEN_ stmt_list ELSE_ stmt_list ENDIF_ { $$ = makeNode(IF, VOID, $3, $6, $8, "IF");}
+    | IF_ '(' expr ')' THEN_ stmt_list ENDIF_ { $$ = makeNode(IF, VOID, $3, $6, NULL, "IF");}
+
+WhileStmt: WHILE_ '(' expr ')' DO_ stmt_list ENDWHILE_ { $$ = makeNode(WHILE, VOID, $3, NULL, $6, "WHILE");}
+
+expr : expr PLUS_ expr		{$$ = makeNode(OPERATOR, INTEGER, $1, NULL, $3, "+");}
+	| expr MINUS_ expr		{$$ = makeNode(OPERATOR, INTEGER, $1, NULL, $3, "-");}
+	| expr MUL_ expr		{$$ = makeNode(OPERATOR, INTEGER, $1, NULL, $3, "*");}
+	| expr DIV_ expr		{$$ = makeNode(OPERATOR, INTEGER, $1, NULL, $3, "/");}
+	| expr LT_ expr			{$$ = makeNode(OPERATOR, BOOLEAN, $1, NULL, $3, "<");}
+	| expr GT_ expr			{$$ = makeNode(OPERATOR, BOOLEAN, $1, NULL, $3, ">");}
+	| expr LE_ expr			{$$ = makeNode(OPERATOR, BOOLEAN, $1, NULL, $3, "<=");}
+	| expr GE_ expr			{$$ = makeNode(OPERATOR, BOOLEAN, $1, NULL, $3, ">=");}
+	| expr NE_ expr			{$$ = makeNode(OPERATOR, BOOLEAN, $1, NULL, $3, "!=");}
+	| expr EQ_ expr			{$$ = makeNode(OPERATOR, BOOLEAN, $1, NULL, $3, "==");}
+	| '(' expr ')' 			{$$ = $2;}
+	| NUM_					{$$ = $1;}
+	| ID_					{$$ = $1;}
+
+%%
+
+void yyerror(char const *s)
+{
+    printf("yyerror %s: %s",s,yytext);
+}
+
+
+int main(void) 
+{
+    yyin=fopen("input.txt","r");
+	yyparse();
+	
+	return 0;
+}
